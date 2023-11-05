176536f051c97cea5633f470d9bffef624cfccdb
sektionschef <the.stevee@gmail.com>
2023-11-05 15:56:21 +0100

    split strokePath

--- strokePath.js
+++ strokePath.js
@@ -17,35 +17,77 @@ class strokePath {
         this.start.y = this.start.y + gaussianRandAdj(0, this.posStd);
         this.end.y = this.end.y + gaussianRandAdj(0, this.posStd);
 
-        // var ShapeLine = {
-        //     x: 400,
-        //     y: 600
-        // }
-        // var interPoint = getIntersectionPoint(this.start, this.end, { x: 0, y: 0 }, ShapeLine);
-        // // console.log(interPoint);
-
-
-        this.startEnd = vectorSub(this.start, this.end);  // vector of diff
-
-
-        this.controlA = {
-            x: this.start.x + this.startEnd.x / this.lineSegment + gaussianRandAdj(0, 1),
-            y: this.start.y + this.startEnd.y / this.lineSegment + gaussianRandAdj(0, 1),
+        this.shapeLine = {
+            x: 400,
+            y: 600
         }
-        this.controlB = {
-            x: this.end.x - this.startEnd.x / this.lineSegment + gaussianRandAdj(0, 1),
-            y: this.end.y - this.startEnd.y / this.lineSegment + gaussianRandAdj(0, 1)
+
+        // if there is an intersection point
+        this.interPoint = getIntersectionPoint(this.start, this.end, { x: 0, y: 0 }, this.shapeLine);
+        this.splitSwitch = isOnLine(this.interPoint.x, this.interPoint.y, this.start.x, this.start.y, this.end.x, this.end.y, 1);
+
+        // dummy
+        this.controlA = { x: 0, y: 0 };
+        this.controlB = { x: 0, y: 0 };
+
+        if (this.splitSwitch) {
+            // console.log("intersecting!")
+
+            // form start to interPoint
+            this.startInterPoint = vectorSub(this.start, this.interPoint);  // vector of diff
+
+            this.controlStartA = {
+                x: this.start.x + this.startInterPoint.x / this.lineSegment + gaussianRandAdj(0, this.posStd),
+                y: this.start.y + this.startInterPoint.y / this.lineSegment + gaussianRandAdj(0, this.posStd),
+            }
+            this.controlStartB = {
+                x: this.end.x - this.startInterPoint.x / this.lineSegment + gaussianRandAdj(0, this.posStd),
+                y: this.end.y - this.startInterPoint.y / this.lineSegment + gaussianRandAdj(0, this.posStd)
+            }
+
+            // form interPoint end
+            this.interPointEnd = vectorSub(this.interPoint, this.end);  // vector of diff
+
+            this.controlEndA = {
+                x: this.interPoint.x + this.interPointEnd.x / this.lineSegment + gaussianRandAdj(0, this.posStd),
+                y: this.interPoint.y + this.interPointEnd.y / this.lineSegment + gaussianRandAdj(0, this.posStd),
+            }
+            this.controlEndB = {
+                x: this.end.x - this.interPointEnd.x / this.lineSegment + gaussianRandAdj(0, this.posStd),
+                y: this.end.y - this.interPointEnd.y / this.lineSegment + gaussianRandAdj(0, this.posStd)
+            }
+
+        } else {
+            // console.log("no intersection with shape -> one continuous path.")
+            this.startEnd = vectorSub(this.start, this.end);  // vector of diff
+
+            this.controlA = {
+                x: this.start.x + this.startEnd.x / this.lineSegment + gaussianRandAdj(0, this.posStd),
+                y: this.start.y + this.startEnd.y / this.lineSegment + gaussianRandAdj(0, this.posStd),
+            }
+            this.controlB = {
+                x: this.end.x - this.startEnd.x / this.lineSegment + gaussianRandAdj(0, this.posStd),
+                y: this.end.y - this.startEnd.y / this.lineSegment + gaussianRandAdj(0, this.posStd)
+            }
         }
 
     }
 
     showPath() {
 
-        this.newpath = document.createElementNS('http://www.w3.org/2000/svg', "path");
+        if (this.splitSwitch) {
+            this.showSplitPath();
+        } else {
+            this.showContinuousPath();
+        }
 
-        this.newpath.setAttributeNS(null, "id", "pathIdD");
-        // newpath.setAttributeNS(null, "d", "M 10 10 C 20 20, 40 20, 50 10");
-        this.newpath.setAttributeNS(null, "d", `M 
+        // this.showDebug()
+    }
+
+    showContinuousPath() {
+        this.newPath = document.createElementNS('http://www.w3.org/2000/svg', "path");
+        this.newPath.setAttributeNS(null, "id", "pathIdD");
+        this.newPath.setAttributeNS(null, "d", `M 
             ${this.start.x} 
             ${this.start.y} 
             C 
@@ -56,72 +98,124 @@ class strokePath {
             ${this.end.x} 
             ${this.end.y}
             `);
-        // newpath.setAttributeNS(null, "stroke", "black");
-        this.newpath.setAttributeNS(null, "stroke", this.strokeColor);
-        this.newpath.setAttributeNS(null, "stroke-width", 0.5);
-        this.newpath.setAttributeNS(null, "opacity", 1);
-        this.newpath.setAttributeNS(null, "fill", "none");
-
-        // DEBUG
-        // debugStart = document.createElementNS('http://www.w3.org/2000/svg', "circle");
-        // debugStart.setAttributeNS(null, "id", "start");
-        // debugStart.setAttributeNS(null, "cx", this.start.x);
-        // debugStart.setAttributeNS(null, "cy", this.start.y);
-        // debugStart.setAttributeNS(null, "r", "3");
-        // debugStart.setAttributeNS(null, "stroke", "blue");
-        // debugStart.setAttributeNS(null, "stroke-width", 2);
-        // debugStart.setAttributeNS(null, "opacity", 1);
-        // debugStart.setAttributeNS(null, "fill", "none");
-
-        // debugEnd = document.createElementNS('http://www.w3.org/2000/svg', "circle");
-        // debugEnd.setAttributeNS(null, "id", "end");
-        // debugEnd.setAttributeNS(null, "cx", this.end.x);
-        // debugEnd.setAttributeNS(null, "cy", this.end.y);
-        // debugEnd.setAttributeNS(null, "r", "3");
-        // debugEnd.setAttributeNS(null, "stroke", "red");
-        // debugEnd.setAttributeNS(null, "stroke-width", 2);
-        // debugEnd.setAttributeNS(null, "opacity", 1);
-        // debugEnd.setAttributeNS(null, "fill", "none");
-
-        // debugControlA = document.createElementNS('http://www.w3.org/2000/svg', "circle");
-        // debugControlA.setAttributeNS(null, "id", "controlA");
-        // debugControlA.setAttributeNS(null, "cx", controlA.x);
-        // debugControlA.setAttributeNS(null, "cy", controlA.y);
-        // debugControlA.setAttributeNS(null, "r", "3");
-        // debugControlA.setAttributeNS(null, "stroke", "pink");
-        // debugControlA.setAttributeNS(null, "stroke-width", 2);
-        // debugControlA.setAttributeNS(null, "opacity", 1);
-        // debugControlA.setAttributeNS(null, "fill", "none");
-
-        // debugControlB = document.createElementNS('http://www.w3.org/2000/svg', "circle");
-        // debugControlB.setAttributeNS(null, "id", "controlB");
-        // debugControlB.setAttributeNS(null, "cx", controlB.x);
-        // debugControlB.setAttributeNS(null, "cy", controlB.y);
-        // debugControlB.setAttributeNS(null, "r", "3");
-        // debugControlB.setAttributeNS(null, "stroke", "pink");
-        // debugControlB.setAttributeNS(null, "stroke-width", 2);
-        // debugControlB.setAttributeNS(null, "opacity", 1);
-        // debugControlB.setAttributeNS(null, "fill", "none");
-
-        // debugInterPoint = document.createElementNS('http://www.w3.org/2000/svg', "circle");
-        // debugInterPoint.setAttributeNS(null, "id", "intersect");
-        // debugInterPoint.setAttributeNS(null, "cx", interPoint.x);
-        // debugInterPoint.setAttributeNS(null, "cy", interPoint.y);
-        // debugInterPoint.setAttributeNS(null, "r", "3");
-        // debugInterPoint.setAttributeNS(null, "stroke", "cyan");
-        // debugInterPoint.setAttributeNS(null, "stroke-width", 1);
-        // debugInterPoint.setAttributeNS(null, "opacity", 1);
-        // debugInterPoint.setAttributeNS(null, "fill", "none");
+        // newPath.setAttributeNS(null, "stroke", "black");
+        this.newPath.setAttributeNS(null, "stroke", this.strokeColor);
+        this.newPath.setAttributeNS(null, "stroke-width", 0.5);
+        this.newPath.setAttributeNS(null, "opacity", 1);
+        this.newPath.setAttributeNS(null, "fill", "none");
 
         const svgNode = document.getElementById('svgNode');
-        svgNode.appendChild(this.newpath);
-        // svgNode.appendChild(debugStart);
-        // svgNode.appendChild(debugEnd);
-        // svgNode.appendChild(debugControlA);
-        // svgNode.appendChild(debugControlB);
+        svgNode.appendChild(this.newPath);
+    }
 
-        // if (isOnLine(interPoint.x, interPoint.y, this.start.x, this.start.y, this.end.x, this.end.y, 1)) {
-        // svgNode.appendChild(debugInterPoint);
-        // }
+    showSplitPath() {
+        this.newPathStart = document.createElementNS('http://www.w3.org/2000/svg', "path");
+        this.newPathStart.setAttributeNS(null, "id", "pathIdD");
+        this.newPathStart.setAttributeNS(null, "d", `M 
+            ${this.start.x} 
+            ${this.start.y} 
+            C 
+            ${this.controlStartA.x} 
+            ${this.controlStartA.y}, 
+            ${this.controlStartB.x} 
+            ${this.controlStartB.y}, 
+            ${this.interPoint.x} 
+            ${this.interPoint.y}
+            `);
+        this.newPathStart.setAttributeNS(null, "stroke", "#15ff00");
+        // this.newPathStart.setAttributeNS(null, "stroke", this.strokeColor);
+        this.newPathStart.setAttributeNS(null, "stroke-width", 0.5);
+        this.newPathStart.setAttributeNS(null, "opacity", 1);
+        this.newPathStart.setAttributeNS(null, "fill", "none");
+
+
+        this.newPathEnd = document.createElementNS('http://www.w3.org/2000/svg', "path");
+        this.newPathEnd.setAttributeNS(null, "id", "pathIdD");
+        this.newPathEnd.setAttributeNS(null, "d", `M 
+            ${this.interPoint.x} 
+            ${this.interPoint.y} 
+            C 
+            ${this.controlEndA.x} 
+            ${this.controlEndA.y}, 
+            ${this.controlEndB.x} 
+            ${this.controlEndB.y}, 
+            ${this.end.x} 
+            ${this.end.y}
+            `);
+        this.newPathEnd.setAttributeNS(null, "stroke", "#e100ff");
+        // this.newPathEnd.setAttributeNS(null, "stroke", this.strokeColor);
+        this.newPathEnd.setAttributeNS(null, "stroke-width", 0.5);
+        this.newPathEnd.setAttributeNS(null, "opacity", 1);
+        this.newPathEnd.setAttributeNS(null, "fill", "none");
+
+
+
+        const svgNode = document.getElementById('svgNode');
+        svgNode.appendChild(this.newPathStart);
+        svgNode.appendChild(this.newPathEnd);
+    }
+
+    showDebug() {
+
+        // DEBUG
+        this.debugStart = document.createElementNS('http://www.w3.org/2000/svg', "circle");
+        this.debugStart.setAttributeNS(null, "id", "start");
+        this.debugStart.setAttributeNS(null, "cx", this.start.x);
+        this.debugStart.setAttributeNS(null, "cy", this.start.y);
+        this.debugStart.setAttributeNS(null, "r", "3");
+        this.debugStart.setAttributeNS(null, "stroke", "blue");
+        this.debugStart.setAttributeNS(null, "stroke-width", 2);
+        this.debugStart.setAttributeNS(null, "opacity", 1);
+        this.debugStart.setAttributeNS(null, "fill", "none");
+
+        this.debugEnd = document.createElementNS('http://www.w3.org/2000/svg', "circle");
+        this.debugEnd.setAttributeNS(null, "id", "end");
+        this.debugEnd.setAttributeNS(null, "cx", this.end.x);
+        this.debugEnd.setAttributeNS(null, "cy", this.end.y);
+        this.debugEnd.setAttributeNS(null, "r", "3");
+        this.debugEnd.setAttributeNS(null, "stroke", "red");
+        this.debugEnd.setAttributeNS(null, "stroke-width", 2);
+        this.debugEnd.setAttributeNS(null, "opacity", 1);
+        this.debugEnd.setAttributeNS(null, "fill", "none");
+
+        this.debugControlA = document.createElementNS('http://www.w3.org/2000/svg', "circle");
+        this.debugControlA.setAttributeNS(null, "id", "controlA");
+        this.debugControlA.setAttributeNS(null, "cx", this.controlA.x);
+        this.debugControlA.setAttributeNS(null, "cy", this.controlA.y);
+        this.debugControlA.setAttributeNS(null, "r", "3");
+        this.debugControlA.setAttributeNS(null, "stroke", "pink");
+        this.debugControlA.setAttributeNS(null, "stroke-width", 2);
+        this.debugControlA.setAttributeNS(null, "opacity", 1);
+        this.debugControlA.setAttributeNS(null, "fill", "none");
+
+        this.debugControlB = document.createElementNS('http://www.w3.org/2000/svg', "circle");
+        this.debugControlB.setAttributeNS(null, "id", "controlB");
+        this.debugControlB.setAttributeNS(null, "cx", this.controlB.x);
+        this.debugControlB.setAttributeNS(null, "cy", this.controlB.y);
+        this.debugControlB.setAttributeNS(null, "r", "3");
+        this.debugControlB.setAttributeNS(null, "stroke", "pink");
+        this.debugControlB.setAttributeNS(null, "stroke-width", 2);
+        this.debugControlB.setAttributeNS(null, "opacity", 1);
+        this.debugControlB.setAttributeNS(null, "fill", "none");
+
+        this.debugInterPoint = document.createElementNS('http://www.w3.org/2000/svg', "circle");
+        this.debugInterPoint.setAttributeNS(null, "id", "intersect");
+        this.debugInterPoint.setAttributeNS(null, "cx", this.interPoint.x);
+        this.debugInterPoint.setAttributeNS(null, "cy", this.interPoint.y);
+        this.debugInterPoint.setAttributeNS(null, "r", "3");
+        this.debugInterPoint.setAttributeNS(null, "stroke", "cyan");
+        this.debugInterPoint.setAttributeNS(null, "stroke-width", 1);
+        this.debugInterPoint.setAttributeNS(null, "opacity", 1);
+        this.debugInterPoint.setAttributeNS(null, "fill", "none");
+
+        const svgNode = document.getElementById('svgNode');
+        if (this.splitSwitch) {
+            svgNode.appendChild(this.debugInterPoint);
+        } else {
+            svgNode.appendChild(this.debugStart);
+            svgNode.appendChild(this.debugEnd);
+            svgNode.appendChild(this.debugControlA);
+            svgNode.appendChild(this.debugControlB);
+        }
     }
 }
